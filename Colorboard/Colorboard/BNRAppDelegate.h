183d5c08716ed2f114686e8e3dda90f4fbb60f8a
Kris Kata <kata.krzysztof@gmail.com>
2015-09-29 18:35:41 -0500

    initial commit

---
+++ Colorboard/Colorboard/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Colorboard
+//
+//  Created by Kris Kata on 9/29/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
