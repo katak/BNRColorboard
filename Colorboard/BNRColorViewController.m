6081416759c96047032e7378e437801c1e9499ee
Kris Kata <kata.krzysztof@gmail.com>
2015-09-29 20:47:26 -0500

    Update Add view with color inputs
     - implemented view controller to update background color

--- Colorboard/BNRColorViewController.m
+++ Colorboard/BNRColorViewController.m
@@ -8,7 +8,27 @@
 
 #import "BNRColorViewController.h"
 
+@interface BNRColorViewController ()
+
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+
+@property (nonatomic, weak) IBOutlet UISlider *redSlider;
+@property (nonatomic, weak) IBOutlet UISlider *greenSlider;
+@property (nonatomic, weak) IBOutlet UISlider *blueSlider;
+
+@end
+
 @implementation BNRColorViewController
+
+- (IBAction)changeColor:(id)sender
+{
+    float red = self.redSlider.value;
+    float green = self.greenSlider.value;
+    float blue = self.blueSlider.value;
+    UIColor *newColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    
+    self.view.backgroundColor = newColor;
+}
 
 - (IBAction)dismiss:(id)sender
 {
